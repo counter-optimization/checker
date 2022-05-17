0b4cbd238bddf73b45f83aeeffd0ecea12885159
Michael Flanders <mkf727@cs.washington.edu>
2022-05-17 12:05:37 -0700

    slightly more thorough aliasing test

--- x86_64_aliasing_tests.c
+++ x86_64_aliasing_tests.c
@@ -8,17 +8,24 @@ main(int argc, char** argv) {
   uint16_t short_zeros = 0;
   uint8_t bytes_ones = ~0;
 
+  uint64_t high_bit = ~(~0ull >> 1);
+  uint64_t second_highest_bit = high_bit >> 1;
+
   uint64_t result = 0;
   
   asm inline("xorq %%rax, %%rax\n"
+	     "movq %4, %%rax\n"
 	     "movl %1, %%eax\n"
+	     "movq %5, %%rax\n"
 	     "movw %2, %%ax\n"
 	     "movb %3, %%al\n"
 	     "movq %%rax, %0\n"
 	     : "=r" (result)
 	     : "ir" (long_ones),
 	       "ir" (short_zeros),
-	       "ir" (bytes_ones)
+	       "ir" (bytes_ones),
+	       "ir" (high_bit),
+	       "ir" (second_highest_bit)
 	     : "rax", "eax", "ax", "ah", "al");
 
   printf("result is: %#016llx\n", result);
