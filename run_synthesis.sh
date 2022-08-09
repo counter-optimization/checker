c33e4bffdb9c79c1a50909de2df2f42f5e97523d
Michael Flanders <mkf727@cs.washington.edu>
2022-08-09 16:33:16 +0000

    edit shell script for running synth to not use macros

--- run_synthesis.sh
+++ run_synthesis.sh
@@ -5,18 +5,19 @@ elif [[ ! -d log ]]; then
     mkdir log
 fi
 
-start=1
+start=2
 end=18
 
 for insn_seq_len in `seq ${start} ${end}`; do
     echo Running insn_seq_len: $insn_seq_len
 
     # generate the hardcoded insn sequence file
-    subbed_file_name=synth/synth-comp-simp-defenses-macrod-${insn_seq_len}.rkt
-    cat synth/synth-comp-simp-defenses-macrod.rkt \
-        | sed "s/REPLACE_ME/${insn_seq_len}/" > ${subbed_file_name}
+    subbed_file_name=synth/synth-comp-simp-defenses-macrod.rkt
+    # cat synth/synth-comp-simp-defenses-macrod.rkt \
+    #     | sed "s/REPLACE_ME/${insn_seq_len}/" > ${subbed_file_name}
 
-    racket ${subbed_file_name} ${insn_seq_len} &>./log/hard-coded-synthesis-seq-len-${insn_seq_len}-`date | tr ' ' '-'`.log &
+    log_file=./log/${insn_seq_len}-seq-len-synthesis-post-testing-`date "+%F-%T" | sed s/:/-/g`.log
+    racket ${subbed_file_name} ${insn_seq_len} &> ${log_file} &
     pids[$i]=$!
     echo Done starting insn_seq_len: $insn_seq_len
 done
