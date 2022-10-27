da51194669268e033d33f4891c31fe44df1d1136
Alexandra Michael <aemichae@cs.washington.edu>
2022-10-27 11:01:59 -0700

    Verification attempt for add32 comp simp

--- synth/verify.rkt
+++ synth/verify.rkt
@@ -8,20 +8,33 @@
 
 (provide (all-defined-out))
 
-(define attempt
+; (define attempt
+;   (list
+;    (sub-r/m32-imm32 eax (bv (expt 2 31) 32))
+;    (sub-r/m32-imm32 eax (bv (expt 2 31) 32))
+;    (setz dl)
+;    (sub-r/m64-imm32 rdx (bv (sub1 (expt 2 32)) 32))
+;    (sub-r/m64-imm32 rdx (bv 1 32))
+;    (sub-r/m64-r64 rax rdx)
+;    (sub-r/m64-r64 rcx rax)
+;    (sub-r/m64-r64 rcx rdx)))
+
+; (define spec
+;   (list
+;    (sub-r/m32-r32 ecx eax)))
+
+(define attempt ; add32
   (list
-   (sub-r/m32-imm32 eax (bv (expt 2 31) 32))
-   (sub-r/m32-imm32 eax (bv (expt 2 31) 32))
-   (setz dl)
-   (sub-r/m64-imm32 rdx (bv (sub1 (expt 2 32)) 32))
-   (sub-r/m64-imm32 rdx (bv 1 32))
+   (mov-r64-imm64 rdx (bv (expt 2 32) 64))
    (sub-r/m64-r64 rax rdx)
-   (sub-r/m64-r64 rcx rax)
+   (sub-r/m64-imm32 rcx rdx)
+   (add-r/m64-r64 rcx rax)
+   (mov-r64-imm64 rdx (bv (- (expt 2 33)) 64))
    (sub-r/m64-r64 rcx rdx)))
 
-(define spec
+(define spec    ; add32
   (list
-   (sub-r/m32-r32 ecx eax)))
+   (add-r/m32-r32 ecx eax)))
 
 (define (comp-simp-verify attempt)
   (define spec-cpu (comp-simp:make-x86-64-cpu))
