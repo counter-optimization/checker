5a783d853c63333f7bc55cbf637e2e7644842349
Alexandra Michael <aemichae@cs.washington.edu>
2022-11-02 15:38:07 -0700

    Fixed add32, sub32 transforms

--- serval/serval/x86/register.rkt
+++ serval/serval/x86/register.rkt
@@ -17,7 +17,7 @@
   rax rcx rdx rbx rsp rbp rsi rdi r8 r9 r10 r11 r12 r13 r14 r15
   eax ecx edx ebx esp ebp esi edi r8d r9d r10d r11d r12d r13d r14d r15d
   ax cx dx bx sp bp si di r8w r9w r10w r11w r12w r13w r14w r15w
-  al cl dl
+  al cl dl r10b r11b
   register-indirect register-indirect?)
 
 
@@ -247,6 +247,8 @@
 (define cl (symbol->gpr8 'cl))
 (define al (symbol->gpr8 'al))
 (define dl (symbol->gpr8 'dl))
+(define r10b (symbol->gpr8 'r10b))
+(define r11b (symbol->gpr8 'r11b))
 
 
 ; displacement
--- synth/arith-transforms.rkt
+++ synth/arith-transforms.rkt
@@ -10,14 +10,17 @@
 
 (define attempt-sub32
   (list
+   (mov-r/m64-r64 r10 rax) ; save rax
    (sub-r/m32-imm32 eax (bv (expt 2 31) 32))
    (sub-r/m32-imm32 eax (bv (expt 2 31) 32))
-   (setz dl)
-   (sub-r/m64-imm32 rdx (bv (sub1 (expt 2 32)) 32))
-   (sub-r/m64-imm32 rdx (bv 1 32))
-   (sub-r/m64-r64 rax rdx)
+   (setz r11b)
+   (sub-r/m64-imm32 r11 (bv (sub1 (expt 2 32)) 32))
+   (sub-r/m64-imm32 r11 (bv 1 32))
+   (sub-r/m64-r64 rax r11)
    (sub-r/m64-r64 rcx rax)
-   (sub-r/m64-r64 rcx rdx)))
+   (sub-r/m64-r64 rcx r11)
+   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
+   (mov-r/m64-r64 rax r10))) ; restore rax
 
 (define spec-sub32
   (list
@@ -25,12 +28,15 @@
 
 (define attempt-add32
   (list
-   (mov-r64-imm64 rdx (bv (expt 2 32) 64))
-   (sub-r/m64-r64 rax rdx)
-   (sub-r/m64-r64 rcx rdx)
-   (add-r/m64-r64 rcx rax)
-   (mov-r64-imm64 rdx (bv (- (expt 2 33)) 64))
-   (sub-r/m64-r64 rcx rdx)))
+   (mov-r/m64-r64 r10 rax) ; save rax
+   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
+   (mov-r/m32-r32 eax eax) ; zero top 32 bits of eax
+   (mov-r64-imm64 r11 (bv (expt 2 32) 64))
+   (sub-r/m64-r64 rax r11)
+   (sub-r/m64-r64 rcx r11)
+   (add-r/m64-r64 rcx rax) ; perform add
+   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
+   (mov-r/m64-r64 rax r10))) ; restore rax
 
 (define spec-add32
   (list
