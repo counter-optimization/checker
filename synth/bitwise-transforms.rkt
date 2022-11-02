673406213adf467cdff9158de7eb16fadc88e47e
Alexandra Michael <aemichae@cs.washington.edu>
2022-11-02 15:53:38 -0700

    Comp simp transforms for or32, xor32

--- synth/bitwise-transforms.rkt
+++ synth/bitwise-transforms.rkt
@@ -23,3 +23,35 @@
 (define spec-and32    ; and32
   (list
    (and-r/m32-r32 ecx eax)))
+
+(define attempt-or32
+  (list
+   (mov-r/m64-r64 r10 rax) ; save rax
+   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
+   (mov-r/m32-r32 eax eax) ; zero top 32 bits of eax
+   (mov-r64-imm64 r11 (bv (expt 2 33) 64))
+   (sub-r/m64-r64 rax r11)
+   (sub-r/m64-r64 rcx r11)
+   (or-r/m64-r64 rcx rax) ; perform and
+   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
+   (mov-r/m64-r64 rax r10))) ; restore rax
+
+(define spec-or32
+  (list
+   (or-r/m32-r32 ecx eax)))
+
+(define attempt-xor32
+  (list
+   (mov-r/m64-r64 r10 rax) ; save rax
+   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
+   (mov-r/m32-r32 eax eax) ; zero top 32 bits of eax
+   (mov-r64-imm64 r11 (bv (expt 2 33) 64))
+   (sub-r/m64-r64 rax r11)
+   (sub-r/m64-r64 rcx r11)
+   (xor-r/m64-r64 rcx rax) ; perform and
+   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
+   (mov-r/m64-r64 rax r10))) ; restore rax
+
+(define spec-xor32
+  (list
+   (xor-r/m32-r32 ecx eax)))
