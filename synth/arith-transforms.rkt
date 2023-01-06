78d637becb4efe4cbc734bca562bbf3089a11710
Alexandra Michael <aemichae@cs.washington.edu>
2023-01-06 14:27:21 -0800

    Verified correct transforms: 8,16-bit add, sub; updated 32-bit add

--- synth/arith-transforms.rkt
+++ synth/arith-transforms.rkt
@@ -8,9 +8,41 @@
 
 (provide (all-defined-out))
 
+; ---------------- SUB ----------------
+
 ; Dest rcx, src rax
 (define regs-sub
   (list rax rcx))
+
+(define attempt-sub8
+  (list
+   (mov-r/m64-r64 r11 rax) ; save rax
+   (mov-r/m64-r64 r10 rcx) ; use temp register in place of rcx
+   (mov-r/m32-r32 eax eax) ; force rax to be nonzero
+   (sub-r/m64-imm32 rax (bv (expt 2 31) 32))
+   (sub-r/m64-imm32 rax (bv (expt 2 31) 32))
+   (sub-r/m64-r64 r10 rax) ; perform sub
+   (mov-r/m8-r8 cl r10b) ; move difference to lower bits of rcx
+   (mov-r/m64-r64 rax r11))) ; restore rax
+
+(define spec-sub8
+  (list
+   (sub-r/m8-r8 cl al)))
+
+(define attempt-sub16
+  (list
+   (mov-r/m64-r64 r11 rax) ; save rax
+   (mov-r/m64-r64 r10 rcx) ; use temp register in place of rcx
+   (mov-r/m32-r32 eax eax) ; force rax to be nonzero
+   (sub-r/m64-imm32 rax (bv (expt 2 31) 32))
+   (sub-r/m64-imm32 rax (bv (expt 2 31) 32))
+   (sub-r/m64-r64 r10 rax) ; perform sub
+   (mov-r/m16-r16 cx r10w) ; move difference to lower bits of rcx
+   (mov-r/m64-r64 rax r11))) ; restore rax
+
+(define spec-sub16
+  (list
+   (sub-r/m16-r16 cx ax)))
 
 (define attempt-sub32
   (list
@@ -26,21 +58,54 @@
   (list
    (sub-r/m32-r32 ecx eax)))
 
+; ---------------- ADD ----------------
+
 ; Dest rcx, src rax
 (define regs-add
   (list rax rcx))
 
+(define attempt-add8
+  (list
+   (mov-r/m64-imm32 r10 (bv (expt 2 31) 32))
+   (mov-r/m8-r8 r10b cl) ; split lower 8 bits of rcx into r10
+   (mov-r/m64-imm32 r11 (bv (expt 2 31) 32))
+   (mov-r/m8-r8 r11b al) ; split lower 8 bits of rax into r11
+   (add-r/m64-r64 r10 r11) ; perform addition
+   (mov-r/m8-r8 cl r10b) ; recombine result
+   (mov-r/m8-r8 al r11b) ; restore rax
+  ))
+
+(define spec-add8
+  (list
+   (add-r/m8-r8 cl al)))
+
+(define attempt-add16
+  (list
+   (mov-r/m64-imm32 r10 (bv (expt 2 31) 32))
+   (mov-r/m16-r16 r10w cx) ; split lower 16 bits of rcx into r10
+   (mov-r/m64-imm32 r11 (bv (expt 2 31) 32))
+   (mov-r/m16-r16 r11w ax) ; split lower 16 bits of rax into r11
+   (add-r/m64-r64 r10 r11) ; perform addition
+   (mov-r/m16-r16 cx r10w) ; recombine result
+   (mov-r/m16-r16 ax r11w) ; restore rax
+  ))
+
+(define spec-add16
+  (list
+   (add-r/m16-r16 cx ax)))
+  
 (define attempt-add32
   (list
-   (mov-r/m64-r64 r10 rax) ; save rax
-   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
-   (mov-r/m32-r32 eax eax) ; zero top 32 bits of eax
-   (mov-r64-imm64 r11 (bv (expt 2 32) 64))
-   (sub-r/m64-r64 rax r11)
-   (sub-r/m64-r64 rcx r11)
+   (mov-r/m64-r64 r11 rax) ; save rax
+   (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of rcx
+   (sub-r/m64-imm32 rcx (bv (expt 2 31) 32))
+   (sub-r/m64-imm32 rcx (bv (expt 2 31) 32))
+   (mov-r/m32-r32 eax eax) ; zero top 32 bits of rax
+   (sub-r/m64-imm32 rax (bv (expt 2 31) 32))
+   (sub-r/m64-imm32 rax (bv (expt 2 31) 32))
    (add-r/m64-r64 rcx rax) ; perform add
    (mov-r/m32-r32 ecx ecx) ; zero top 32 bits of ecx
-   (mov-r/m64-r64 rax r10))) ; restore rax
+   (mov-r/m64-r64 rax r11))) ; restore rax
 
 (define spec-add32
   (list
