88530e241ff52626f3f390b0d166ea4304f8de11
Alexandra Michael <aemichae@cs.washington.edu>
2023-01-25 17:38:18 -0800

    mul32 transform verified

--- synth/arith-transforms.rkt
+++ synth/arith-transforms.rkt
@@ -176,3 +176,61 @@
 (define spec-add64
   (list
    (add-r/m64-r64 rcx rax)))
+
+; ---------------- MUL ----------------
+
+; Dest rdx:rax, src rcx
+(define regs-mul
+  (list rdx rax rcx))
+
+; (define attempt-mul16
+;   (list
+;   ; (mul-r/m16 cx)
+;    (mov-r/m64-r64 r11 rax) ; save rax to r11
+;    (mov-r/m64-imm32 r10 (bv 0 32)) ; zero r10
+;    (mov-r/m16-r16 r10w cx) ; use r10 as operand
+;    (mov-r/m32-imm32 eax (bv 0 32)) ; zero rax
+;    (mov-r/m16-r16 ax r11w) ; move lower 16 bits back as operand
+;    (mul-r/m32 r10d) ; multiply
+;    (mov-r/m16-r16 r11w ax) ; move lower product to r11
+;    (shr-r/m32-imm8 eax (bv 16 8)) ; shift over upper product
+;    (mov-r/m16-r16 dx ax) ; move upper product to dx
+;    (mov-r/m64-r64 rax r11) ; set rax to correct result
+;   ))
+
+; (define spec-mul16
+;   (list
+;    (mul-r/m16 cx)))
+
+(define attempt-mul32
+  (list
+  ; safe to use rdx for scratch; it'll be overwritten later regardless
+   (mov-r64-imm64 rdx (bv (expt 2 63) 64))
+   ; set r10 = ecx + 2^63, r11 = eax + 2^63 (equivalent to ecx/eax - 2^63)
+   (mov-r/m32-r32 r10d ecx)
+   (sub-r/m64-r64 r10 rdx)
+   (mov-r/m32-r32 r11d eax)
+   (sub-r/m64-r64 r11 rdx)
+   ; replace rax with eax + 2^63
+   (mov-r/m64-r64 rax r11)
+   ; perform 64-bit mul of modified operands. This gives us
+   ; (eax + 2^63) * (ecx + 2^63)
+   ; = eax * ecx + eax * 2^63 + ecx * 2^63 + 2^126
+   (mul-r/m64 r10)
+   ; product is in rax. subtract shifted r11, r10 to get expected value
+   ; (equivalent to subtracting eax * 2^63 and ecx * 2^63)
+   (shl-r/m64-imm8 r10 (bv 63 8))
+   (shl-r/m64-imm8 r11 (bv 63 8))
+   (sub-r/m64-r64 rax r10)
+   (sub-r/m64-r64 rax r11)
+   ; set edx to contain upper 32 bits of the product (from rax)
+   (mov-r/m64-r64 rdx rax)
+   (mov-r/m16-imm16 dx (bv 1 16)) ; mask lower bits for safe shift
+   (shr-r/m64-imm8 rdx (bv 32 8))
+   ; clear upper 32 bits of rax
+   (mov-r/m32-r32 eax eax)
+  ))
+
+(define spec-mul32
+  (list
+   (mul-r/m32 ecx)))
