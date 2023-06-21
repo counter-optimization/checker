73781b91347c6bf7f8aae1be692242d364516210
Michael Flanders <mkf727@cs.washington.edu>
2023-06-21 21:58:19 +0000

    fix bugs with live-in and live-out analysis on flags

--- parse_checker_alert_csv.py
+++ parse_checker_alert_csv.py
@@ -31,6 +31,12 @@ def get_flags_live_in(csv_row_dict):
     flags_live_in_csv_str = csv_row_dict['flags_live_in']
     flags_live_in = str.split(flags_live_in_csv_str, sep=',')
     return set(flags_live_in)
+
+def alist_to_dict(alist):
+    d = {}
+    for (k,v) in alist:
+        d[k] = v
+    return d
     
 if __name__ == '__main__':
     cs_opcodes = set()
@@ -44,11 +50,25 @@ if __name__ == '__main__':
 
     num_cs_warns = 0
     num_ss_warns = 0
-    
-    for csv_file_name in sys.argv[1:]:
+
+    cs_warns_1 = set()
+    ss_warns_1 = set()
+
+    cs_warns_2 = set()
+    ss_warns_2 = set()
+
+    # (true, false) -> (false, true) -> (false, false)
+    is_warn_set_1 = True
+    is_warn_set_2 = False
+
+    fieldnames = ["subroutine_name","mir_opcode","addr","rpo_idx","tid","problematic_operands","left_operand","right_operand","live_flags","is_live","alert_reason","description","flags_live_in"]
+
+    for idx, csv_file_name in enumerate(sys.argv[1:]):
         # calculate mir_opcodes -> live flags for all alerts
         with open(csv_file_name, mode="r") as csv_file:
             reader = csv.DictReader(csv_file)
+
+            field_names = reader.fieldnames
 
             logger.info(f"Processing csv file: {csv_file_name}")
             for row in reader:
@@ -58,12 +78,23 @@ if __name__ == '__main__':
 
                 subs.add(row['subroutine_name'])
 
-
                 if row['rpo_idx'] == "":
                     logger.critical(f"Row ({row}) has no rpo_idx")
                 
                 if has_flags_live_in(row) or has_live_flags(row):
                     logger.debug(f"Row ({row}) has live flags out: {row['live_flags']}, live flags in: {row['flags_live_in']}")
+
+                if is_warn_set_1 and not is_warn_set_2:
+                    if is_comp_simp_warn(row):
+                        cs_warns_1.add(frozenset(row.items()))
+                    if is_silent_store_warn(row):
+                        ss_warns_1.add(frozenset(row.items()))
+
+                if is_warn_set_2 and not is_warn_set_1:
+                    if is_comp_simp_warn(row):
+                        cs_warns_2.add(frozenset(row.items()))
+                    if is_silent_store_warn(row):
+                        ss_warns_2.add(frozenset(row.items()))
 
                 if is_comp_simp_warn(row):
                     num_cs_warns += 1
@@ -97,6 +128,14 @@ if __name__ == '__main__':
                     cur_addrs.add(addrs)
                     opcodes_to_addrs_live_out[opcode] = cur_addrs
 
+        if is_warn_set_2 and not is_warn_set_1:
+            is_warn_set_1 = False
+            is_warn_set_2 = False
+                    
+        if is_warn_set_1 and not is_warn_set_2:
+            is_warn_set_1 = False
+            is_warn_set_2 = True
+
     print("Flag live out info:")
     for opcode, live_flags in opcodes_to_live_flags.items():
         addrs = opcodes_to_addrs_live_out[opcode]
@@ -129,3 +168,23 @@ if __name__ == '__main__':
 
     print(f"total transforms needed: {len(cs_opcodes) + len(ss_opcodes)}")
     print(f"CS warns: {num_cs_warns}, SS warns: {num_ss_warns}")
+
+    alert_addrs_now = set()
+    print(f"num alerts in cs_warns_2: {len(cs_warns_2)}")
+    print(f"num alerts in cs_warns_1: {len(cs_warns_1)}")
+    for alert2 in cs_warns_2:
+        alert2 = alist_to_dict(alert2)
+        alert_addrs_now.add(alert2['addr'])
+
+    with open("asymmetric_alert_diff.csv", "w") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        missed_alerts = set()
+        for alert1 in cs_warns_1:
+            alert1_d = alist_to_dict(alert1)
+            if not alert1_d['addr'] in alert_addrs_now:
+                missed_alerts.add(alert1)
+            
+        for missed_alert in missed_alerts:
+            writer.writerow(alist_to_dict(missed_alert))
+            
