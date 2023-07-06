9b9bd6db987ed92af0a7aefb7584352c93f1eb01
Alexandra Michael <aemichae@cs.washington.edu>
2023-07-05 17:16:20 -0700

    IMUL64rr sets CF

--- synth/verify.rkt
+++ synth/verify.rkt
@@ -48,6 +48,6 @@
 (module+ main
   (displayln "running verification...")
   ; (define cex (verify (comp-simp-verify attempt-mul16-p12 spec-mul16-p12 (list ax cx))))
-  (define cex (verify (comp-simp-verify attempt-imul64-rri8 spec-imul64-rri8 (list rcx rax))))
+  (define cex (verify (comp-simp-verify attempt-imul64-rr-p5 spec-imul64-rr-p5 (list rax) (list 'CF))))
   (displayln "done")
   cex)
